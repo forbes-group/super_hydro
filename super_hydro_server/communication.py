579553c954a1f96bc40734c99dcfeaea40a9b04e
Michael McNeil Forbes <michael.forbes+bitbucket@gmail.com>
2018-08-17 13:48:17 -0700

    WIP: skeleton for custom send/recv with size
    --HG--
    branch : client_server

--- super_hydro_server/communication.py
+++ super_hydro_server/communication.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 import json
 import socket
 import logging
@@ -6,6 +7,22 @@ def log(msg, level=logging.ERROR):
     """Log msg to the logger."""
     # Get logger each time so handlers are properly dealt with
     logging.getLogger(__name__).log(level=level, msg=msg)
+
+
+@contextmanager
+def log_task(msg, _level=[0]):
+    indent = " " * 2 * _level[0]
+    msg = indent + msg
+    log(msg + "...")
+    try:
+        _level[0] += 1
+        yield
+        log(msg + ". Done.")
+    except:
+        log(msg + ". Failed!", level=logging.ERROR)
+        raise
+    finally:
+        _level[0] -= 1
 
 
 class Communicator(object):
@@ -20,15 +37,27 @@ class Communicator(object):
         return json.loads(self.get_raw(buffsize))
 
     def send(self, msg):
-        self.sock.send(msg.encode())
+        try:
+            msg = msg.encode()
+        except:
+            pass
+        size = len(msg)
+        ####self.sock.send(size)
+        self.sock.send(msg)
         
-    def request(self, name, msg=None):
-        self.sock.send(name.encode())
+    def recv(self):
+        ####size = int(self.sock.recv(1))
+        return self.sock.recv(size).decode()
+
+    def request(self, name, user_message=None):
+        msg = name.encode()
+        self.send(msg)
+        
         error_check = self.get_raw(128)
         if error_check == "ERROR":
-            if msg is None:
-                msg = "Request {} unsuccessful".format(name)
-            print(msg)
+            if user_message is None:
+                user_message = "Request {} unsuccessful".format(name)
+            print(user_message)
             return False
         else:
             return True
