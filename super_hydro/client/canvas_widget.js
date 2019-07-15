d4e6d15186d442a4c1069999afb8e2f90b294db0
Michael McNeil Forbes <michael.forbes+bitbucket@gmail.com>
2019-07-15 00:11:43 -0700

    ENH: Added preliminary canvas widget.

---
+++ super_hydro/client/canvas_widget.js
@@ -0,0 +1,82 @@
+// -*- mode: js; js-indent-level: 2; -*-
+require.undef('canvas_widget');
+
+define('canvas_widget', ["@jupyter-widgets/base"], function(widgets) {
+  var CanvasView = widgets.DOMWidgetView.extend({
+  
+    // Render the view.
+    render: function() {
+      this.canvas = document.createElement("canvas");
+      this._ctx = this.canvas.getContext('2d');
+      this.el.appendChild(this.canvas);
+
+      // Background for rendering on Safari etc.
+      this._bg_canvas = document.createElement("canvas");
+      this._bg_ctx = this._bg_canvas.getContext('2d');
+
+      this.update()
+      
+      // Python -> JavaScript update
+      //this.model.on('change:width', this.update, this);
+      //this.model.on('change:height', this.update, this);
+      //this.model.on('change:_rgba', this.update, this);
+      
+      // JavaScript -> Python update
+      //this.value.onchange = this.value_changed.bind(this);
+    },
+    
+    update: function() {
+      var _data = this.model.get('_rgba');
+      if (_data && _data.byteLength) {
+        var _raw_data = new Uint8ClampedArray(_data.buffer);
+        var _width = this.model.get('_image_width');
+        this._image_data = new ImageData(_raw_data, _width);
+        //requestAnimationFrame(this.draw_image.bind(this));
+        this.draw_image();
+      }
+    },
+
+    draw_image: function() {
+      // Draws the image data on the canvas, scaling up if the size
+      // does not match.
+      var image_data = this._image_data;
+      var width = this.model.get('width');
+      var height = this.model.get('height');
+      var im_width = image_data.width;
+      var im_height = image_data.height;
+      debugger;
+      if (0 == width && 0 == height) {
+        width = im_width;
+        height = im_height;
+      } else if (0 == height) {
+        // Preserve aspect ratio
+        height = im_height * width / im_width;
+      } else if (0 == width) {
+        // Preserve aspect ratio
+        width = im_width * height / im_height;
+      }
+      
+      if (width == im_width && height == im_height) {
+        // Simply draw.
+        this.canvas.width = width;
+        this.canvas.height = height;                 
+        this._ctx.putImageData(image_data, 0, 0);
+      } else {
+        // Pre-render on background canvas, then scale
+        this._bg_canvas.width = im_width;
+        this._bg_canvas.height = im_height;
+        this._bg_ctx.putImageData(image_data, 0, 0);
+
+        this.canvas.width = width;
+        this.canvas.height = height;                 
+        this._ctx.drawImage(this._bg_canvas,
+                            0, 0, im_width, im_height,
+                            0, 0, width, height);
+      }
+    }
+  });
+  
+  return {
+    CanvasView: CanvasView
+  };
+});
---
+++ super_hydro/client/canvas_widget.py
@@ -0,0 +1,73 @@
+"""Initial version of a Canvas-based widget.
+
+This version displays the required javascript in the notebook so does
+not require any installation.
+"""
+import os.path
+
+import numpy as np
+
+from traitlets import Unicode, Bool, validate, TraitError, Instance, Int, Bytes
+from ipywidgets import DOMWidget, register
+from ipywidgets.widgets.trait_types import bytes_serialization
+
+
+_JS_FILE = __file__[:-3] + '.js'
+
+
+@register
+class Canvas(DOMWidget):
+    """HTML5 Canvas Object.
+
+    Examples
+    --------
+    >>> from matplotlib import cm
+    >>> canvas = Canvas()
+    >>> display(canvas)
+    >>> A = np.random.random((64, 32))
+    >>> canvas.rgba = cm.viridis(A.T, bytes=True)
+    """
+    _view_name = Unicode('CanvasView').tag(sync=True)
+    _view_module = Unicode('canvas_widget').tag(sync=True)
+    _view_module_version = Unicode('0.1.0').tag(sync=True)
+
+    _rgba = Bytes(help="RGBA image data").tag(sync=True, **bytes_serialization)
+    _image_width = Int(help="Image width").tag(sync=True)
+
+    # Attributes
+    width = Int(0, help="Width of canvas").tag(sync=True)
+    height = Int(0, help="Height of canvas").tag(sync=True)
+
+    indexing = Unicode(
+        'xy', help="Indexing: 'xy' (faster) or 'ij'.  See np.meshgrid")
+
+    @property
+    def rgba(self):
+        """RGBA bytes array from a colormap.
+
+        Note: This should should be in ij imaging with axis 0
+        corresponding to x (wide) and axis 1 corresponding to y
+        (height).
+
+        For example:
+
+            canvas.rgba = cm.viridis(data, bytes=True)
+        """
+        return self._rgba_data
+
+    @rgba.setter
+    def rgba(self, rgba_data):
+        self._rgba_data = rgba_data
+        if self.indexing == 'ij':
+            self._image_width = rgba_data.shape[0]
+            # Swap axes to go from ij indexing to HTML convention.
+            self._rgba = np.swapaxes(rgba_data, 0, 1).tobytes()
+        else:
+            self._image_width = rgba_data.shape[1]
+            self._rgba = rgba_data.tobytes()
+
+
+def display_js():
+    from IPython.display import Javascript, display
+    with open(_JS_FILE) as f:
+        display(Javascript(f.read()))
