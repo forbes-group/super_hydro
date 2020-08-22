86cce280aa7ec135a5065cfdfaba7ce20ef92cc3
Michael McNeil Forbes <michael.forbes+bitbucket@gmail.com>
2020-08-21 18:03:59 -0700

    CLI,WEB: Simplified density display allowing colormaps.
    --HG--
    branch : flask

--- super_hydro/client/static/js/app_func.js
+++ super_hydro/client/static/js/app_func.js
@@ -1,28 +1,41 @@
 // Animation framework function.
-function drawCustom(data, nx, ny) {
-  sh.x_space = width/nx;
-  sh.y_space = height/ny;
-  var tmpcanvas = document.createElement("canvas");
-  tmpcanvas.width = nx;
-  tmpcanvas.height = ny;
-  var tmpctx = tmpcanvas.getContext("2d");
+function drawCustom(rgba, nx, ny) {
+  var image_data = new ImageData(rgba, nx);
+  var im_width = nx;
+  var im_height = ny;
+      
+  if (0 == width && 0 == height) {
+    width = im_width;
+    height = im_height;
+  } else if (0 == height) {
+    // Preserve aspect ratio
+    height = im_height * width / im_width;
+  } else if (0 == width) {
+    // Preserve aspect ratio
+    width = im_width * height / im_height;
+  }
 
-  var dataBinding = dataContainer.selectAll("custom")
-                                       .data(data);
-  var enterSel = dataBinding.enter()
-                                  .append("custom");
-  dataBinding = dataBinding.merge(enterSel);
+  if (width == im_width && height == im_height) {
+    // Simply draw.
+    canvasDensity.width = width;
+    canvasDensity.height = height;                 
+    ctxDensity.putImageData(image_data, 0, 0);
+  } else {
+    // Pre-render on background canvas, then scale
+    var tmpcanvas = document.createElement("canvas");
+    var tmpctx = tmpcanvas.getContext("2d");
+    tmpcanvas.width = im_width;
+    tmpcanvas.height = im_height;
+    tmpctx.putImageData(image_data, 0, 0);
 
-  ctxDensity.clearRect(0, 0, chartDensity.attr("width"), chartDensity.attr("height"));
-
-  var elements = dataContainer.selectAll("custom");
-
-  elements.each(function(d, i) {
-    tmpctx.fillStyle = "rgb("+ d +","+ d +","+(255-d)+")";
-    tmpctx.fillRect(Math.floor(i/nx), (i%ny),
-                     1, 1)
-  });
-  ctxDensity.drawImage(tmpcanvas, 0, 0, nx, ny, 0, 0, width, height)
+    chartDensity.width = width;
+    chartDensity.height = height;                 
+    canvasDensity.width = width;
+    canvasDensity.height = height;                 
+    ctxDensity.drawImage(tmpcanvas,
+                         0, 0, im_width, im_height,
+                         0, 0, width, height);
+  } 
 }
 
 //Finger and Potential layer function.
