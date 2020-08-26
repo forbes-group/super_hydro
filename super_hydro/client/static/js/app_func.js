7251a806237376f386af28c8a0698a5d8f06cec9
Kyle Elsasser <kyle.elsasser@wsu.edu>
2020-08-26 13:05:31 -0700

    CLI,WEB: Updated dynamic elements in HTML.
    --HG--
    branch : flask

--- super_hydro/client/static/js/app_func.js
+++ super_hydro/client/static/js/app_func.js
@@ -1,6 +1,40 @@
+// setParam() ties with 'param_up' socket event to update changes in user
+// controllable parameter values.
+function setParam(name, value) {
+ sh.Param = {};
+ sh.Param[name] = value;
+ socket.emit('set_param', {data: sh.namespace, param: sh.Param});
+};
+
+// setLogParam() ties with 'log_param_up' socket event to update changes in
+// user controllable logarithmic scale parameter values.
+function setLogParam(name, value) {
+  sh.Param = {};
+  sh.Param[name] = Math.pow(10, value);
+  socket.emit('set_log_param', {data: sh.namespace, param: sh.Param})
+}
+
+// doAction() passes action calls to the server.
+function doAction(name) {
+ socket.emit('do_action', {data: sh.namespace, name: name});
+};
+
+// This section provides mouseclick/touchscreen interaction for finger
+// potential.
+function getCursorPosition(canvas, event) {
+ const rect = canvas.getBoundingClientRect()
+ sh.fy = (event.clientY - rect.top);
+ sh.fx = (event.clientX - rect.left);
+ const fPos = {'xy0' : [sh.fy/(rect.bottom-rect.top),
+                        sh.fx/(rect.right-rect.left)]}
+ socket.emit('finger', {position: fPos})
+};
+
 // Animation framework function.
 function drawCustom(rgba, nx, ny) {
   var image_data = new ImageData(rgba, nx);
+  var width = chartDensity.width;
+  var height = chartDensity.height;
   var im_width = nx;
   var im_height = ny;
 
@@ -17,8 +51,8 @@ function drawCustom(rgba, nx, ny) {
 
   if (width == im_width && height == im_height) {
     // Simply draw.
-    canvasDensity.width = width;
-    canvasDensity.height = height;
+    chartDensity.width = width;
+    chartDensity.height = height;
     ctxDensity.putImageData(image_data, 0, 0);
   } else {
     // Pre-render on background canvas, then scale
@@ -28,11 +62,8 @@ function drawCustom(rgba, nx, ny) {
     tmpcanvas.height = im_height;
     tmpctx.putImageData(image_data, 0, 0);
 
-    baseDensity.attr("width", width)
-                .attr("height", height);
-    //chartDensity.height = height;
-    canvasDensity.width = width;
-    canvasDensity.height = height;
+    chartDensity.width = width;
+    chartDensity.height = height;
     ctxDensity.drawImage(tmpcanvas,
                          0, 0, im_width, im_height,
                          0, 0, width, height);
@@ -41,19 +72,20 @@ function drawCustom(rgba, nx, ny) {
 
 //Finger and Potential layer function.
 function drawFinger(fx, fy, vx, vy) {
-  vxNew = vx*width;
-  vyNew = vy*height;
+  vxNew = vx*chartFinger.width;
+  vyNew = vy*chartFinger.height;
+  potSize = document.getElementById('finger_V0_mu').value
 
-  ctxFinger.clearRect(0, 0, chartFinger.attr("width"), chartFinger.attr("height"));
+  ctxFinger.clearRect(0, 0, chartFinger.width, chartFinger.height);
 
-  ctxFinger.beginPath();
-  ctxFinger.strokeStyle = "White";
-  ctxFinger.arc(fx, fy, 5, 0, Math.PI*2, true);
-  ctxFinger.stroke();
   ctxFinger.beginPath();
   ctxFinger.fillStyle = "Black";
-  ctxFinger.arc(vxNew, vyNew, 5, 0, Math.PI*2, true);
+  ctxFinger.arc(fx, fy, 5, 0, Math.PI*2, true);
   ctxFinger.fill();
+  ctxFinger.beginPath();
+  ctxFinger.strokeStyle = "White";
+  ctxFinger.arc(vxNew, vyNew, 8, 0, Math.PI*2, true);
+  ctxFinger.stroke();
   ctxFinger.beginPath();
   ctxFinger.moveTo(fx, fy);
   ctxFinger.lineTo(vxNew, vyNew);
